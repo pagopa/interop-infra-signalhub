f01a0d222c68b374069ee2d4420390c377380a89
Francesco Carlucci <francesco.carlucci@accenture.com>
2023-10-27 12:36:28 +0200

    NTRP-8 removed metric-server

--- src/main/50-monitoring.tf
+++ src/main/50-monitoring.tf
@@ -6,50 +6,50 @@ resource "kubernetes_namespace" "monitoring" {
   depends_on = [module.eks]
 }
 
-resource "helm_release" "metrics_server" {
-  name       = "metrics-server"
-  chart      = "metrics-server"
-  repository = "https://kubernetes-sigs.github.io/metrics-server"
-  version    = var.helm_metrics_server_version
-  namespace  = kubernetes_namespace.monitoring.id
-
-  set {
-    name  = "containerPort"
-    value = 4443
-  }
-
-  set {
-    name  = "args[0]"
-    value = "--kubelet-insecure-tls"
-  }
-}
-
-resource "kubectl_manifest" "metrics_server_sg" {
-  yaml_body = yamlencode({
-    apiVersion = "vpcresources.k8s.aws/v1beta1"
-    kind       = "SecurityGroupPolicy"
-
-    metadata = {
-      name      = "metrics-server"
-      namespace = kubernetes_namespace.monitoring.id
-    }
-
-    spec = {
-      podSelector = {
-        matchLabels = {
-          "app.kubernetes.io/instance" = "metrics-server"
-          "app.kubernetes.io/name"     = "metrics-server"
-        }
-      }
-
-      securityGroups = {
-        groupIds = [
-          module.eks.cluster_primary_security_group_id
-        ]
-      }
-    }
-  })
-}
+#resource "helm_release" "metrics_server" {
+#  name       = "metrics-server"
+#  chart      = "metrics-server"
+#  repository = "https://kubernetes-sigs.github.io/metrics-server"
+#  version    = var.helm_metrics_server_version
+#  namespace  = kubernetes_namespace.monitoring.id
+#
+#  set {
+#    name  = "containerPort"
+#    value = 4443
+#  }
+#
+#  set {
+#    name  = "args[0]"
+#    value = "--kubelet-insecure-tls"
+#  }
+#}
+#
+#resource "kubectl_manifest" "metrics_server_sg" {
+#  yaml_body = yamlencode({
+#    apiVersion = "vpcresources.k8s.aws/v1beta1"
+#    kind       = "SecurityGroupPolicy"
+#
+#    metadata = {
+#      name      = "metrics-server"
+#      namespace = kubernetes_namespace.monitoring.id
+#    }
+#
+#    spec = {
+#      podSelector = {
+#        matchLabels = {
+#          "app.kubernetes.io/instance" = "metrics-server"
+#          "app.kubernetes.io/name"     = "metrics-server"
+#        }
+#      }
+#
+#      securityGroups = {
+#        groupIds = [
+#          module.eks.cluster_primary_security_group_id
+#        ]
+#      }
+#    }
+#  })
+#}
 
 resource "helm_release" "prometheus" {
   name       = "prometheus"
