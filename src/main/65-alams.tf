3685671077ae746cddaf550278455192d89c26e3
francesco.carlucci <$43u6Kt4>
2023-12-11 11:27:46 +0100

    NTRP-8 added http 5xx alarms

--- src/main/65-alams.tf
+++ src/main/65-alams.tf
@@ -57,3 +57,41 @@ resource "aws_cloudwatch_metric_alarm" "push_target_response_time" {
 }
 
 
+resource "aws_cloudwatch_metric_alarm" "pull_target_http_5xx" {
+  alarm_name          = "${local.project}-Pull-Http-5xx"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = lookup(var.fiveXXs_thresholds, "period")
+  statistic           = lookup(var.fiveXXs_thresholds, "statistic")
+  threshold           = lookup(var.fiveXXs_thresholds, "threshold")
+  actions_enabled     = "false"
+  alarm_description   = "Trigger an alert when error 5xx occurs in Pull EService"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    TargetGroup  = data.aws_lb_target_group.target_group_pull.arn
+    LoadBalancer = data.aws_lb.load_balancer.arn
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "push_target_http_5xx" {
+  alarm_name          = "${local.project}-Push-Http-5xx"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = lookup(var.fiveXXs_thresholds, "period")
+  statistic           = lookup(var.fiveXXs_thresholds, "statistic")
+  threshold           = lookup(var.time_response_thresholds, "threshold")
+  actions_enabled     = "false"
+  alarm_description   = "Trigger an alert when error 5xx occurs in Pull EService"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    TargetGroup  = data.aws_lb_target_group.target_group_push.arn
+    LoadBalancer = data.aws_lb.load_balancer.arn
+  }
+}
+
