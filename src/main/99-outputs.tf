d0c2b17d8ae4b243538d63678402954280773fd0
francesco.carlucci <$43u6Kt4>
2023-12-13 10:33:29 +0100

    NTRP-8 review outputs

--- src/main/99-outputs.tf
+++ src/main/99-outputs.tf
@@ -1,13 +1,59 @@
+#output "hosted_zone_id_env_signalhub_interop_pagopa_it" {
+#  description = "<env>.signalhub.interop.pagopa.it hosted zone id"
+#  value       = aws_route53_zone.env_signalhub_interop_pagopa_it.zone_id
+#}
+#
+#output "ns_env_signalhub_interop_pagopa_it" {
+#  description = "NS for <env>.signalhub.interop.pagopa.it"
+#  value       = aws_route53_zone.env_signalhub_interop_pagopa_it.name_servers
+#}
+#
+#output "sqs" {
+#  value = module.sqs.queue_url
+#}
+#
+#output "redis" {
+#  value = module.redis.elasticache_replication_group_primary_endpoint_address
+#}
+#
+#output "cluster_iam_role_name" {
+#  value = module.eks.cluster_iam_role_name
+#}
+#
+#
+#output "aws_caller_identity_current_arn" {
+#  value = data.aws_caller_identity.current.arn
+#}
+#
+#output "aws_caller_identity_current_id" {
+#  value = data.aws_caller_identity.current.id
+#}
+#
+#
+#
+#output "interop_client_key_id" {
+#  value = aws_kms_key.interop_client_key.key_id
+#}
+#
+#output "fargate_profiles" {
+#  value = module.eks.fargate_profiles
+#}
+#
+#output "fargate_profiles_role_names" {
+#  value = local.fargate_log_role_names
+#}
+#
+#output "eks_cluster_endpoint" {
+#  value = module.eks.cluster_endpoint
+#}
+#
+#output "cloudwatch_log_group_name" {
+#  value = module.eks.cloudwatch_log_group_name
+#}
 
-# ROUTE53 OUTPUTS
-output "hosted_zone_id_env_signalhub_interop_pagopa_it" {
-  description = "<env>.signalhub.interop.pagopa.it hosted zone id"
-  value       = aws_route53_zone.env_signalhub_interop_pagopa_it.zone_id
-}
-
-output "ns_env_signalhub_interop_pagopa_it" {
-  description = "NS for <env>.signalhub.interop.pagopa.it"
-  value       = aws_route53_zone.env_signalhub_interop_pagopa_it.name_servers
+output "load_balancer_host" {
+  description = "DNS name of load balancer"
+  value       = data.aws_lb.load_balancer.dns_name
 }
 
 output "signalhub_cluster_name" {
@@ -15,47 +61,6 @@ output "signalhub_cluster_name" {
   value       = module.eks.cluster_name
 }
 
-output "sqs" {
-  value = module.sqs.queue_url
-}
-
-output "redis" {
-  value = module.redis.elasticache_replication_group_primary_endpoint_address
-}
-
-output "cluster_iam_role_name" {
-  value = module.eks.cluster_iam_role_name
-}
-
-
-output "aws_caller_identity_current_arn" {
-  value = data.aws_caller_identity.current.arn
-}
-
-output "aws_caller_identity_current_id" {
-  value = data.aws_caller_identity.current.id
-}
-
 output "interop_client_key_arn" {
   value = aws_kms_key.interop_client_key.arn
-}
-
-output "interop_client_key_id" {
-  value = aws_kms_key.interop_client_key.key_id
-}
-
-output "fargate_profiles" {
-  value = module.eks.fargate_profiles
-}
-
-output "fargate_profiles_role_names" {
-  value = local.fargate_log_role_names
-}
-
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cloudwatch_log_group_name" {
-  value = module.eks.cloudwatch_log_group_name
 }
